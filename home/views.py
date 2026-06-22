469b5c83c141d0bd1812693fd5bd56e73a47d642
Churchillk <churchilkodhiambo@gmail.com>
2026-06-22 21:35:49 +0300

    dynamic listing

--- home/views.py
+++ home/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from properties.models import Property
 
-class HomeView(TemplateView):
+class HomeView(ListView):
+    model = Property
+    context_object_name = "properties"
+    paginate_by = 3
     template_name = "home/index.html"
