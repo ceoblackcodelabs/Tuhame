720df43f135a0ad1cfdb221ff7097ee73fe218b5
Churchillk <churchilkodhiambo@gmail.com>
2026-06-22 23:56:05 +0300

    map

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,7 @@ app_name = "home"
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
+    path("properties/listing/", PropertiesListView.as_view(), name="properties"),
+    path("property/listing/<slug:slug>/", PropertiesDetailView.as_view(), name="about_property"),
+    path("property/map-list/", PropertyMapSearchListView.as_view(), name="property_map")
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from properties.models import Property
 
@@ -8,3 +8,22 @@ class HomeView(ListView):
     context_object_name = "properties"
     paginate_by = 3
     template_name = "home/index.html"
+
+
+class PropertiesListView(ListView):
+    model = Property
+    context_object_name = "properties"
+    paginate_by = 9
+    template_name = "home/properties/properties.html"
+
+class PropertiesDetailView(DetailView):
+    model = Property
+    context_object_name = "property"
+    template_name = "home/properties/about_property.html"
+
+
+class PropertyMapSearchListView(ListView):
+    model = Property
+    context_object_name = "properties"
+    paginate_by = 3
+    template_name = "home/properties/map.html"
